28bc49d14116d89a444fca0099d56130a329fb3b
concentratedmuesli <concentratedmuesli@gmail.com>
2025-10-19 15:06:50 +0200

    Stay logged in on reload

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -10,12 +10,22 @@ export function useAuth() {
 // TODO: and use only redux to check if the user is logged in
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  const storedUsername = localStorage.getItem('username');
+  if (!user && storedUsername) {
+    setUser({ storedUsername });
+  }
+
   const login = (user) => {
-    setUser({ user });
+    setUser({user});
+    localStorage.setItem("username", user);
   };
+
   const logout = () => {
     setUser(null);
+    localStorage.removeItem("username");
   };
+
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
       {children}
