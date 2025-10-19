a6c117ab4f0bb0dd91eadf0959b9d598fa127834
concentratedmuesli <concentratedmuesli@gmail.com>
2025-10-19 18:50:22 +0200

    Show if sending a message worked or not

--- src/Kontakt.js
+++ src/Kontakt.js
@@ -1,25 +1,45 @@
 import './Kontakt.css';
 import { useRef, useState } from 'react';
-import { useAuth } from './AuthProvider';
 import { postMessage } from './API';
+import { useAuth } from './AuthProvider';
 
 export default function Kontakt() {
   const titleRef = useRef(null);
   const bodyRef = useRef(null);
   const [postingError, setPostingError] = useState(null);
   const { logout } = useAuth();
+  const [missingTitle, setMissingTitle] = useState(false);
+  const [missingBody, setMissingBody] = useState(false);
+  const [postingSuccess, setPostingSuccess] = useState(false);
 
   function sendMessage(event) {
     event.preventDefault();
+    setPostingSuccess(false);
+    if (titleRef.current.value.trim().length === 0) {
+      setMissingTitle(true);
+    } else {
+      setMissingTitle(false);
+    }
+    if (bodyRef.current.value.trim().length === 0) {
+      setMissingBody(true);
+    } else {
+      setMissingBody(false);
+    }
+    if (
+      titleRef.current.value.trim().length === 0 ||
+      bodyRef.current.value.trim().length === 0
+    ) {
+      return;
+    }
     const sendData = async () => {
       try {
         postMessage(titleRef.current.value, bodyRef.current.value, logout);
-      } catch (err) {
-        setPostingError(true);
-      } finally {
         setPostingError(false);
+        setPostingSuccess(true);
         titleRef.current.value = '';
         bodyRef.current.value = '';
+      } catch (err) {
+        setPostingError(true);
       }
     };
     sendData();
@@ -31,11 +51,23 @@ export default function Kontakt() {
       <form className="cardContent">
         <label className="label">Betreff</label>
         <input ref={titleRef} className="input" />
+        {missingTitle ? <div>Das Betreff kann nicht leer sein.</div> : <></>}
         <label className="label">Nachricht</label>
         <textarea ref={bodyRef} rows={5} className="input" />
+        {missingBody ? <div>Die Nachricht kann nicht leer sein.</div> : <></>}
         <button className="button" onClick={sendMessage}>
           Schicken
         </button>
+        {postingError ? (
+          <div>Deine Nachricht konnte nicht geschickt werden!</div>
+        ) : (
+          <></>
+        )}
+        {postingSuccess ? (
+          <div>Deine Nachricht wurde erfolgreich gesendet!</div>
+        ) : (
+          <></>
+        )}
       </form>
     </div>
   );
