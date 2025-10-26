7ba62316fdd7f9d99910e44b5e7b6aa1688fc626
concentratedmuesli <concentratedmuesli@gmail.com>
2025-10-26 13:17:42 +0100

    Add vercel analytics

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from './authorization/AuthProvider';
 import { ProtectedRoute } from './authorization/ProtectedRoute';
 import { Logout } from './authorization/Logout';
 import { Loading } from './Loading.js';
+import { Analytics } from '@vercel/analytics/react';
 
 // Seiten werden erst geladen, wenn sie wirklich benötigt werden.
 // Reduziert die initiale Bundle-Größe und beschleunigt den ersten Start
@@ -18,61 +19,64 @@ const Login = lazy((load) => import('./authorization/Login.js'));
 // Suspense und Loading zeigen Ladeindikator, während Lazy-Komponenten nachgeladen werden
 function App() {
   return (
-    <BrowserRouter>
-      <nav className={styles.nav}>
-        <NavLink to="/" className={`${styles.logo} ${styles.navHeader}`}>
-          GlowTrack
-        </NavLink>
-        <NavLink to="/ueber-uns" className={styles.navHeader}>
-          Über uns
-        </NavLink>
-        <NavLink to="/kontakt" className={styles.navHeader}>
-          Kontakt
-        </NavLink>
-        <NavLink
-          to="/logout"
-          className={styles.navHeader}
-          aria-label="Ausloggen"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#444444"
+    <>
+      <BrowserRouter>
+        <nav className={styles.nav}>
+          <NavLink to="/" className={`${styles.logo} ${styles.navHeader}`}>
+            GlowTrack
+          </NavLink>
+          <NavLink to="/ueber-uns" className={styles.navHeader}>
+            Über uns
+          </NavLink>
+          <NavLink to="/kontakt" className={styles.navHeader}>
+            Kontakt
+          </NavLink>
+          <NavLink
+            to="/logout"
+            className={styles.navHeader}
+            aria-label="Ausloggen"
           >
-            <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
-          </svg>
-        </NavLink>
-      </nav>
-      <main>
-        <Suspense fallback={<Loading />}>
-          <AuthProvider>
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <ProtectedRoute>
-                    <Dashboard />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/ueber-uns" element={<UeberUns />} />
-              <Route
-                path="/kontakt"
-                element={
-                  <ProtectedRoute>
-                    <Kontakt />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/login" element={<Login />} />
-              <Route path="/logout" element={<Logout />} />
-            </Routes>
-          </AuthProvider>
-        </Suspense>
-      </main>
-    </BrowserRouter>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#444444"
+            >
+              <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
+            </svg>
+          </NavLink>
+        </nav>
+        <main>
+          <Suspense fallback={<Loading />}>
+            <AuthProvider>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <ProtectedRoute>
+                      <Dashboard />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="/ueber-uns" element={<UeberUns />} />
+                <Route
+                  path="/kontakt"
+                  element={
+                    <ProtectedRoute>
+                      <Kontakt />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="/login" element={<Login />} />
+                <Route path="/logout" element={<Logout />} />
+              </Routes>
+            </AuthProvider>
+          </Suspense>
+        </main>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
 
